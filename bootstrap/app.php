a0c79bce2579a5277e6d3cdde4b5aadf5198d28e
Henry Nkuke <henricsanyu@gmail.com>
2024-11-23 22:55:48 +0300

    cors fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
 
         $middleware->append(\App\Http\Middleware\ExcludeCors::class);
+        $middleware->validateCsrfTokens(except: [
+            'jpesa/callback',
+        ]);
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
